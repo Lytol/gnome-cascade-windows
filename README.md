5137e4adac55f533d4b8d10246c07cbd8a22142e
Brian Smith <brian.e.smith@gmail.com>
2017-06-13 19:12:54 -0700

    Initial import of basic gnome extension for cascading windows

---
+++ README.md
@@ -0,0 +1,17 @@
+Cascade Windows Gnome Extension
+-------------------------------
+
+A Gnome 3.2+ extension that simply rearranges windows in the current desktop into a cascade (stacked, mostly overlapping, moving from top-left to bottom-right)
+
+Notes
+-----
+
+- Always uses the current workspace
+- Only uses the primary monitor workspaces
+
+
+TODO
+----
+
+- Better icon for the menu bar
+- Allow use of a global hotkey
---
+++ extension.js
@@ -0,0 +1,117 @@
+const St = imports.gi.St;
+const Main = imports.ui.main;
+const Lang = imports.lang;
+
+let cascader, button;
+
+const GUTTER_SIZE = 50;
+
+const WindowCascader = new Lang.Class({
+  Name: "WindowCascader.WindowCascader",
+
+  _init: function() {},
+
+  cascade: function() {
+    global.log("[DEBUG] cascade");
+
+    let windows = this._cascadableWindows();
+
+    for(let i = 0; i < windows.length; i++) {
+      this._updateWindow(windows[i], i);
+    }
+  },
+
+  _primaryMonitor: function() {
+    return global.screen.get_primary_monitor();
+  },
+
+  _currentWorkspace: function() {
+    return global.screen.get_active_workspace();
+  },
+
+  _workArea: function() {
+    return this._currentWorkspace().get_work_area_for_monitor(this._primaryMonitor());
+  },
+
+  _gutterWidth: function() {
+    return GUTTER_SIZE;
+  },
+
+  _gutterHeight: function() {
+    return GUTTER_SIZE;
+  },
+
+  _cascadableWindows: function() {
+    let windows = [];
+    let windowActors = global.get_window_actors();
+
+    for (let i = 0; i < windowActors.length; i++) {
+      let win = windowActors[i].meta_window;
+
+      if (this._isCascadable(win)) {
+        windows.push(win);
+      }
+    }
+
+    return windows;
+  },
+
+  _isCascadable: function(win) {
+    if (win.is_on_all_workspaces()) {
+      return false;
+    }
+
+    if (win.get_monitor() != this._primaryMonitor()) {
+      return false;
+    }
+
+    if (!win.located_on_workspace(this._currentWorkspace())) {
+      return false;
+    }
+
+    return true;
+  },
+
+  _updateWindow: function(win, idx) {
+    let x = ((idx+1) * this._gutterWidth()) + this._workArea().x;
+    let y = ((idx+1) * this._gutterHeight()) + this._workArea().y;
+    let width = this._workArea().width - ((this._cascadableWindows().length + 1) * this._gutterWidth());
+    let height = this._workArea().height - ((this._cascadableWindows().length + 1) * this._gutterHeight());
+
+    global.log("X: " + x + " \ Y: " + y + " \ width: " + width + " \ height: " + height);
+    win.move_resize_frame(false, x, y, width, height);
+  }
+});
+
+function _cascade() {
+  if (cascader !== null) {
+    cascader.cascade();
+  }
+}
+
+function init() {
+  button = new St.Bin({
+    style_class: 'panel-button',
+    reactive: true,
+    can_focus: true,
+    x_fill: true,
+    y_fill: false,
+    track_hover: true });
+
+  let icon = new St.Icon({
+    icon_name: 'system-run-symbolic',
+    style_class: 'system-status-icon' });
+
+  button.set_child(icon);
+  button.connect('button-press-event', _cascade);
+}
+
+function enable() {
+  cascader = new WindowCascader();
+  Main.panel._rightBox.insert_child_at_index(button, 0);
+}
+
+function disable() {
+  cascader = null;
+  Main.panel._rightBox.remove_child(button);
+}
